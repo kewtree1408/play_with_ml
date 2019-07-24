25126c1fc9ed42bec3dfc811b9937b0880a6d7f3
Victoria <me@vika.space>
2019-07-24 21:57:39 +0200

    Added tests to cnn.py (one is still failing)

--- cnn/conv.py
+++ cnn/conv.py
@@ -1,9 +1,36 @@
 import numpy as np
+from typing import Generator, Tuple
 
 
 class Conv3x3:
-    def __init__(self, filter_num):
-        self.filter_num = filter_num
+    def __init__(self, filters_amount: int):
+        self.filters_amount = filters_amount
+        self.filter_size = 3 # because Conv3x3, but can be any number
+        # 9 - magic number for Xavier - ? TODO!
+        self.filters = np.random.rand(
+            filters_amount, self.filter_size, self.filter_size
+        ) / 9
 
-        self.filters = np.random.rand(filter_num, 3, 3) / 9
+    @staticmethod
+    def divide_input(input_image: np.ndarray) -> Generator[Tuple[np.ndarray, int, int], None, None]:
+        """input_image is 2D"""
+        height, length = input_image.shape
+        for i in range(height-2):
+            for j in range(length-2):
+                yield input_image[i:i+3, j:j+3], i, j
+
+    @staticmethod
+    def dot_sum(image_part, conv_filter):
+        assert image_part.shape == conv_filter.shape
+        return np.sum(np.multiply(image_part, conv_filter))
+
+    def apply_filters(self, input_image: np.ndarray) -> np.ndarray:
+        height, length = input_image.shape
+        output_data = np.zeros((self.filters_amount, height-2, length-2))
+        import ipdb; ipdb.set_trace()
+        for img_part, i, j in self.divide_input(input_image):
+            output_data[i, j] = np.sum(img_part * self.filters, axis=(1, 2))
+            # for conv_filter in self.filters:
+            #     output_data[i, j] = self.dot_sum(img_part, conv_filter)
+        return output_data
 
---
+++ cnn/tests/test_conv.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+from conv import Conv3x3
+
+# Tests for this example:
+# http://deeplearning.net/software/theano/_images/numerical_no_padding_no_strides.gif
+
+def test_dot_sum():
+    cnn_filter = np.array([
+        [0, 1, 2],
+        [2, 2, 0],
+        [0, 1, 2]
+    ])
+    input_data = np.array([
+        [3, 3, 2],
+        [0, 0, 1],
+        [3, 1, 2],
+    ])
+    assert Conv3x3.dot_sum(input_data, cnn_filter) == 12
+
+def test_divide_input():
+    input_data = np.array([
+        [3, 3, 2, 1, 0],
+        [0, 0, 1, 3, 1],
+        [3, 1, 2, 2, 3],
+        [2, 0, 0, 2, 2],
+        [2, 0, 0, 0, 1],
+    ])
+    expected_shape = 9
+    expected_parts = [
+        (np.array([
+            [3, 3, 2],
+            [0, 0, 1],
+            [3, 1, 2],
+        ]), 0, 0),
+        (np.array([
+            [3, 2, 1],
+            [0, 1, 3],
+            [1, 2, 2],
+        ]), 0, 1),
+        (np.array([
+            [2, 1, 0],
+            [1, 3, 1],
+            [2, 2, 3],
+        ]), 0, 2),
+        (np.array([
+            [0, 0, 1],
+            [3, 1, 2],
+            [2, 0, 0],
+        ]), 1, 0),
+        (np.array([
+            [0, 1, 3],
+            [1, 2, 2],
+            [0, 0, 2],
+        ]), 1, 1),
+        (np.array([
+            [1, 3, 1],
+            [2, 2, 3],
+            [0, 2, 2],
+        ]), 1, 2),
+        (np.array([
+            [3, 1, 2],
+            [2, 0, 0],
+            [2, 0, 0],
+        ]), 2, 0),
+        (np.array([
+            [1, 2, 2],
+            [0, 0, 2],
+            [0, 0, 0],
+        ]), 2, 1),
+        (np.array([
+            [2, 2, 3],
+            [0, 2, 2],
+            [0, 0, 1],
+        ]), 2, 2),
+    ]
+
+    divided_parts = list(Conv3x3.divide_input(input_data))
+    assert len(divided_parts) == expected_shape
+    for res, expected in zip(divided_parts, expected_parts):
+        part_result, i_result, j_result = res
+        part_expected, i_expected, j_expected = expected
+        assert i_result == i_expected
+        assert j_result == j_expected
+        np.testing.assert_array_equal(part_result, part_expected)
+
+
+def test_apply_filters():
+    # TODO: doesn't work -> for tomorrow
+    filters_amount = 1 # depth
+    cn = Conv3x3(filters_amount)
+    # reassign filter for easier testing
+    cn.filters = np.array([[
+        [0, 1, 2],
+        [2, 2, 0],
+        [0, 1, 2]
+    ]])
+    input_data = np.array([
+        [3, 3, 2, 1, 0],
+        [0, 0, 1, 3, 1],
+        [3, 1, 2, 2, 3],
+        [2, 0, 0, 2, 2],
+        [2, 0, 0, 0, 1],
+    ])
+    expected_output = np.array([
+        [12, 12, 17],
+        [10, 17, 19],
+        [9, 6, 14]
+    ])
+    output_result = cn.apply_filters(input_data)
+    np.testing.assert_array_equal(output_result, expected_output)
